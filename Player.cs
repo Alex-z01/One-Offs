f22451b7245688fe93de447d32cce4c8602b0a35
Alex-z01 <69604949+Alex-z01@users.noreply.github.com>
2020-09-08 14:19:32 -0400

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace JsonManager
+{
+    class Player
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        [JsonProperty]
+        public string IGN { get; set; }
+
+        [JsonProperty]
+        public int Gold { get; set; }
+
+        [JsonProperty]
+        public int Level { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Player Data:\n\tIGN: {0}, \n\tGold: {1}, \n\tLevel: {2}", IGN, Gold, Level);
+        }
+    }
+}
+
